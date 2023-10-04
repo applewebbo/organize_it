00ff873f3a16c3171161e85a8076b7bf5163ff48
webbo <e.bonardi@me.com>
2023-10-04 08:06:21 +0200

    added custom user and django-all-auth for #1

--- core/settings/common.py
+++ core/settings/common.py
@@ -25,7 +25,11 @@ ALLOWED_HOSTS: list[str] = env("ALLOWED_HOSTS")
 INSTALLED_APPS = [
     # INTERNAL_APP
     "projects",
+    "accounts",
     # THIRD_PARTY
+    "allauth",
+    "allauth.account",
+    "allauth.socialaccount",
     # CONTRIB
     "django.contrib.admin",
     "django.contrib.auth",
@@ -41,6 +45,7 @@ MIDDLEWARE = [
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "allauth.account.middleware.AccountMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
@@ -122,3 +127,61 @@ MEDIA_URL = "/media/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = "accounts.CustomUser"
+
+# SITES
+
+SITE_ID = 1
+
+# DJANGO-ALLAUTH
+
+AUTHENTICATION_BACKENDS = [
+    # Needed to login by username in Django admin, regardless of `allauth`
+    "django.contrib.auth.backends.ModelBackend",
+    # `allauth` specific authentication methods, such as login by email
+    "allauth.account.auth_backends.AuthenticationBackend",
+]
+
+# ACCOUNT_ADAPTER => default
+# ACCOUNT_AUTHENTICATED_LOGIN_REDIRECTS => default
+ACCOUNT_AUTHENTICATION_METHOD = "email"
+ACCOUNT_CONFIRM_EMAIL_ON_GET = True
+# ACCOUNT_EMAIL_CONFIRMATION_ANONYMOUS_REDIRECT_URL => default
+# ACCOUNT_EMAIL_CONFIRMATION_AUTHENTICATED_REDIRECT_URL => default
+# ACCOUNT_EMAIL_CONFIRMATION_EXPIRE_DAYS => default
+# ACCOUNT_EMAIL_CONFIRMATION_HMAC => default
+ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_EMAIL_VERIFICATION = "mandatory"
+ACCOUNT_EMAIL_SUBJECT_PREFIX = "ORGANIZE IT! - "
+ACCOUNT_DEFAULT_HTTP_PROTOCOL = env("ACCOUNT_DEFAULT_HTTP_PROTOCOL")
+# ACCOUNT_EMAIL_CONFIRMATION_COOLDOWN => default
+# ACCOUNT_EMAIL_MAX_LENGTH => default
+# ACCOUNT_MAX_EMAIL_ADDRESSES => default
+# ACCOUNT_FORMS => default
+# ACCOUNT_LOGIN_ATTEMPTS_LIMIT => default
+# ACCOUNT_LOGIN_ATTEMPTS_TIMEOUT => default
+ACCOUNT_LOGIN_ON_EMAIL_CONFIRMATION = True
+# ACCOUNT_LOGOUT_ON_GET => default
+# ACCOUNT_LOGOUT_ON_PASSWORD_CHANGE => default
+# ACCOUNT_LOGIN_ON_PASSWORD_RESET => default
+# ACCOUNT_LOGOUT_REDIRECT_URL => default
+# ACCOUNT_PASSWORD_INPUT_RENDER_VALUE => default
+ACCOUNT_PRESERVE_USERNAME_CASING = False
+# ACCOUNT_PREVENT_ENUMERATION => default
+# ACCOUNT_RATE_LIMITS => default
+ACCOUNT_SESSION_REMEMBER = True
+# ACCOUNT_SIGNUP_EMAIL_ENTER_TWICE => default
+# ACCOUNT_SIGNUP_FORM_CLASS => default
+ACCOUNT_SIGNUP_PASSWORD_ENTER_TWICE = False
+# ACCOUNT_SIGNUP_REDIRECT_URL => default
+# ACCOUNT_TEMPLATE_EXTENSION => default
+# ACCOUNT_USERNAME_BLACKLIST => default
+# ACCOUNT_UNIQUE_EMAIL => default
+# ACCOUNT_USER_DISPLAY =>  default
+# ACCOUNT_USER_MODEL_EMAIL_FIELD => default
+ACCOUNT_USER_MODEL_USERNAME_FIELD = None
+# ACCOUNT_USERNAME_MIN_LENGTH => default
+ACCOUNT_USERNAME_REQUIRED = False
+# ACCOUNT_USERNAME_VALIDATORS => default
+# SOCIALACCOUNT_* => default
--- core/urls.py
+++ core/urls.py
@@ -20,6 +20,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("accounts/", include("allauth.urls")),
     path("__reload__/", include("django_browser_reload.urls")),
     path("", include("projects.urls", namespace="projects")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
