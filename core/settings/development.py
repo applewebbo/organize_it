0423d5ba6164fea0068976a76cd5412703aebbd6
webbo <e.bonardi@me.com>
2023-10-05 10:35:05 +0200

    added templates for login, logout and mail verification for #1

--- core/settings/development.py
+++ core/settings/development.py
@@ -1,5 +1,7 @@
 from core.settings.common import *  # noqa
 
+# DJANGO-BROWSER-RELOAD
+
 INSTALLED_APPS += [
     # DEVELOPMENT APPS
     "django_browser_reload",
@@ -8,3 +10,8 @@ INSTALLED_APPS += [
 MIDDLEWARE += [
     "django_browser_reload.middleware.BrowserReloadMiddleware",
 ]
+
+
+# MAIL
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
