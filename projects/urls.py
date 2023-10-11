bd38bb8633cad2025f312e9ec4f3ab768bcbb283
webbo <e.bonardi@me.com>
2023-10-11 15:37:37 +0200

    initial project detail view (#3)

--- projects/urls.py
+++ projects/urls.py
@@ -6,4 +6,5 @@ app_name = "projects"
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("projects/<int:project_id>", views.project_detail, name="project_detail"),
 ]
--- projects/views.py
+++ projects/views.py
@@ -1,6 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+
+from .models import Project
 
 
 def home(request):
     context = {}
     return render(request, "projects/index.html", context)
+
+
+def project_detail(request, project_id):
+    project = get_object_or_404(Project, pk=project_id)
+    context = {"project": project}
+    return render(request, "projects/project-detail.html", context)
