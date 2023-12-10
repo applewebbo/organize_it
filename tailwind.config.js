d7a608a3f178b666cd3b3bb4d9293c0b8256a473
WEBBOgrafico <e.bonardi@me.com>
2023-12-10 14:55:29 +0100

    added DaisyUI and tweaked navbar and index page (#42)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./templates/**/*.{html,js}"],
+  content: [
+    "./templates/**/*.{html,js}",
+  ],
   theme: {
     extend: {},
   },
-  plugins: [require("@tailwindcss/forms")],
-}
+  plugins: [
+    require("@tailwindcss/forms"),
+    require("daisyui"),
+  ],
+};
