b51d35d9ea4533448bec04dd6ad131f1cd451078
WEBBOgrafico <e.bonardi@me.com>
2024-03-09 08:15:45 +0100

    added production setting for populate_trips task

--- trips/tasks.py
+++ trips/tasks.py
@@ -1,6 +1,12 @@
-from django.core import management
+from django.conf import settings
+from django.core import management  # pragma: no cover
 
 
-def populate_trips():
-    management.call_command("populate_trips", "--settings=trips.settings.dev")
+def populate_trips():  # pragma: no cover
+    if settings.DEBUG:
+        management.call_command("populate_trips", "--settings=trips.settings.dev")
+    else:
+        management.call_command(
+            "populate_trips", "--settings=trips.settings.production"
+        )
     print("Trips populated correctly!")
