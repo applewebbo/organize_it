1355fe614ba71f08840dfb2e41ae46c854506ae7
WEBBOgrafico <e.bonardi@me.com>
2025-04-07 10:50:12 +0200

    added i18n settings and files, updated django-tailwind-cli settings for #122)

--- core/settings/common.py
+++ core/settings/common.py
@@ -2,6 +2,7 @@ from pathlib import Path
 
 import dj_database_url
 import environ
+from django.utils.translation import gettext_lazy as _
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
@@ -61,6 +62,7 @@ MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.locale.LocaleMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -208,63 +210,22 @@ AUTHENTICATION_BACKENDS = [
     "allauth.account.auth_backends.AuthenticationBackend",
 ]
 
-# ACCOUNT_ADAPTER => default
-# ACCOUNT_AUTHENTICATED_LOGIN_REDIRECTS => default
 ACCOUNT_LOGIN_METHODS = {"email"}
 ACCOUNT_CONFIRM_EMAIL_ON_GET = True
-# ACCOUNT_EMAIL_CONFIRMATION_ANONYMOUS_REDIRECT_URL => default
-# ACCOUNT_EMAIL_CONFIRMATION_AUTHENTICATED_REDIRECT_URL => default
-# ACCOUNT_EMAIL_CONFIRMATION_EXPIRE_DAYS => default
-# ACCOUNT_EMAIL_CONFIRMATION_HMAC => default
 ACCOUNT_SIGNUP_FIELDS = ["email*", "password1*"]
 ACCOUNT_EMAIL_VERIFICATION = "mandatory"
 ACCOUNT_EMAIL_SUBJECT_PREFIX = "ORGANIZE IT! - "
 ACCOUNT_DEFAULT_HTTP_PROTOCOL = env("ACCOUNT_DEFAULT_HTTP_PROTOCOL")
-# ACCOUNT_EMAIL_CONFIRMATION_COOLDOWN => default
-# ACCOUNT_EMAIL_MAX_LENGTH => default
-# ACCOUNT_MAX_EMAIL_ADDRESSES => default
-# ACCOUNT_FORMS => default
-# ACCOUNT_LOGIN_ATTEMPTS_LIMIT => default
-# ACCOUNT_LOGIN_ATTEMPTS_TIMEOUT => default
 ACCOUNT_LOGIN_ON_EMAIL_CONFIRMATION = True
-# ACCOUNT_LOGOUT_ON_GET => default
-# ACCOUNT_LOGOUT_ON_PASSWORD_CHANGE => default
-# ACCOUNT_LOGIN_ON_PASSWORD_RESET => default
-# ACCOUNT_LOGOUT_REDIRECT_URL => default
-# ACCOUNT_PASSWORD_INPUT_RENDER_VALUE => default
 ACCOUNT_PRESERVE_USERNAME_CASING = False
-# ACCOUNT_PREVENT_ENUMERATION => default
-# ACCOUNT_RATE_LIMITS => default
 ACCOUNT_SESSION_REMEMBER = True
-# ACCOUNT_SIGNUP_EMAIL_ENTER_TWICE => default
-# ACCOUNT_SIGNUP_FORM_CLASS => default
-# ACCOUNT_SIGNUP_REDIRECT_URL => default
-# ACCOUNT_TEMPLATE_EXTENSION => default
-# ACCOUNT_USERNAME_BLACKLIST => default
-# ACCOUNT_UNIQUE_EMAIL => default
-# ACCOUNT_USER_DISPLAY =>  default
-# ACCOUNT_USER_MODEL_EMAIL_FIELD => default
 ACCOUNT_USER_MODEL_USERNAME_FIELD = None
-# ACCOUNT_USERNAME_MIN_LENGTH => default
-# ACCOUNT_USERNAME_VALIDATORS => default
-# SOCIALACCOUNT_* => default
 
 
 # DJANGO CRISPY FORMS
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = "tailwind"
-
 CRISPY_TEMPLATE_PACK = "tailwind"
-
-# # ALERT STYLES
-
-# MESSAGE_TAGS = {
-#     messages.DEBUG: "text-gray-800 rounded-lg bg-gray-50 dark:bg-gray-800 dark:text-gray-300",
-#     messages.INFO: " text-blue-800 rounded-lg bg-blue-50 dark:bg-gray-800 dark:text-blue-400",
-#     messages.SUCCESS: "text-green-800 rounded-lg bg-green-50 dark:bg-gray-800 dark:text-green-400",
-#     messages.WARNING: "text-yellow-800 rounded-lg bg-yellow-50 dark:bg-gray-800 dark:text-yellow-300",
-#     messages.ERROR: "text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400",
-# }
 
 # MAPBOX
 
@@ -283,4 +244,16 @@ Q_CLUSTER = {
     "catch_up": False,
 }
 
-TAILWIND_CLI_VERSION = "4.1.2"
+# DJANGO-TAILWIND-CLI
+
+TAILWIND_CLI_SRC_CSS = "static/css/source.css"
+
+# I18N
+LANGUAGES = (
+    ("it", _("Italian")),
+    ("en", _("English")),
+)
+
+LOCALE_PATHS = [
+    BASE_DIR / "locale/",
+]
