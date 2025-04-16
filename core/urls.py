8fd14f79b25b0d38e997b44f17176f83f726ce25
applewebbo <e.bonardi@me.com>
2025-04-16 07:08:22 +0200

    fixed navbar for language_chooser and removed staticfiles (for #122)

--- core/urls.py
+++ core/urls.py
@@ -4,6 +4,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path("i18n/", include("django.conf.urls.i18n")),
     path("admin/", admin.site.urls),
     path("accounts/", include("allauth.urls")),
     path("accounts/", include("accounts.urls", namespace="accounts")),
