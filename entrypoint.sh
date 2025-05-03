f748fd99e549f2cd3369b10935dd81ac539802f2
applewebbo <83427957+applewebbo@users.noreply.github.com>
2025-05-03 15:33:09 +0200

    Tasks cluster (#134)
    * added supervisord for parallel granian and dbworker
    
    * fixed stderr and stdout errors and forward logs to docker's log
    
    * removed log forwarding in entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,13 +15,6 @@ python manage.py tailwind build
 echo "Collecting static files..."
 python manage.py collectstatic --no-input
 
-echo "Starting granian..."
-exec granian "core.wsgi:application" \
-    --host 0.0.0.0 \
-    --port 80 \
-    --interface wsgi \
-    --no-ws \
-    --loop uvloop \
-    --process-name "granian [core]" \
-    --workers 2 \
-    --backpressure 16
+# Start the web server and tasks worker
+echo "Starting supervisord.."
+exec supervisord -c /app/supervisord.conf
