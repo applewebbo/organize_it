85e02ec632c80398d8d69e8212614a835ae236ba
applewebbo <e.bonardi@me.com>
2025-12-09 08:01:48 +0100

    chore: update python 3.13 and dependencies

--- core/settings.py
+++ core/settings.py
@@ -53,7 +53,6 @@ INSTALLED_APPS = [
     "django_extensions",
     "django_htmx",
     "django_tailwind_cli",
-    "template_partials.apps.SimpleAppConfig",
     "django_q",
     "storages",
     "dbbackup",
@@ -109,7 +108,6 @@ TEMPLATES = [
             ],
             "builtins": [
                 "django_cotton.templatetags.cotton",
-                "template_partials.templatetags.partials",
             ],
             "debug": True,
         },
@@ -173,7 +171,6 @@ MEDIA_URL = "/media/"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "accounts.CustomUser"
 
